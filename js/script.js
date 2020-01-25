633e49941da420bad4ebe976436aa0c8e682ce9c
Anton Zuev <wnfgnqpw@gmail.com>
2020-01-25 17:33:43 +0300

    выполнил второй урок

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
-let money, income, addExpenses, deposit, mission, period;
-alert('hello, world!');
-console.log('hello, console!');
+let money = 30000, 
+    income = 'фриланс', 
+    addExpenses = 'Интернет, Бензин, Кофе', 
+    deposit = true, 
+    mission = 200000, 
+    period = 6;
+
+console.log(typeof (money));
+console.log(typeof (income));
+console.log(typeof (deposit));
+
+console.log(addExpenses.length);
+
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = money / 30;
+console.log(budgetDay);
