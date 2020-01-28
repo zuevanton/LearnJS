09556d712d9214d80a5373157aca6f22c6ad52c7
Anton Zuev <wnfgnqpw@gmail.com>
2020-01-28 23:06:41 +0300

    четвертый усложненный

--- js/script.js
+++ js/script.js
@@ -1,34 +1,17 @@
 'use strict';
-// создаем переменные
-let lang = 'ru',
-    ruWeek = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-    enWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-
-// выводим дни недели через if
-console.log('задание 1 а');
-if (lang === 'ru'){
-  ruWeek.forEach(element => console.log(element));
-}
-else enWeek.forEach(element => console.log(element));
-
-// через switch
-console.log('задание 1 b');
-switch(lang) {
-  case 'ru':
-    ruWeek.forEach(element => console.log(element));
-    break;
-  case 'en':
-    enWeek.forEach(element => console.log(element));
+let arg = prompt('введите что нибудь');
+function myFunction(arg){
+  if (arg === null){
+    alert('передана не строка!');
+    arg = prompt('попробуйте еще раз');
+    return myFunction(arg);
+  }
+  arg = arg.trim();
+  if (arg.length > 30) {
+    arg = arg.slice(0, 30);
+    return arg + '...';
+  }
+  return arg;
 }
 
-// через многомерный массив
-console.log('задание 1 c');
-let week = {
-  ru: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-  en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-};
-week[lang].forEach(element => console.log(element));
-
-// задание 2 
-let namePerson = 'Максим',
-    result = namePerson === 'Артем' ? console.log('Директор') : namePerson === 'Максим' ? console.log('преподаватель') : console.log('студент');
+console.log(myFunction(arg));
