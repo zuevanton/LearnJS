6b300df15040d22f4e5468990819501e5fc5f03b
Anton Zuev <wnfgnqpw@gmail.com>
2020-02-04 22:59:10 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,30 +1,9 @@
 'use strict';
 
-let week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
-    div = document.createElement('div'),
+let fullDate = document.querySelector('.short-date'),
+    shortDate = document.querySelector('.long-date'),
     date = new Date();
 
-function createDay(day, num){
-  // если попадаем на субботу и вскр, то выводим их Ж и К
-  if (num === date.getDay() && (date.getDay() === 6 || date.getDay() === 0)){
-    div.innerHTML += '<p><b><i>' + day + '</i></b></p>';
-  }
-  // если суббота или вскр, то делаем их К
-  else if (num === 0 || num === 6){
-    div.innerHTML += '<p><i>' + day + '</i></p>';
-  }
-  // если попадаем на день, то делаем Ж
-  else if(num === date.getDay()){
-    div.innerHTML += '<p><b>' + day + '</b></p>';
-  }
-  // выводим без всего
-  else {
-    div.innerHTML += '<p>' + day + '</p>';
-  }
-}
 
-for(let i = 1; i < week.length; i++){
-  createDay(week[i], i);
-}
-createDay(week[0], 0);
-document.body.append(div);
+    
+// document.body.append(div);
